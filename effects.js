b959d60586fc09fa4d2ec6651df82ffd5f5f80f1
ChandaSarkarCybage <chandas@cybage.com>
2016-08-30 14:56:20 +0530

    Assignment 1 2 3 checked in

--- effects.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    var app = angular.module("indexModule", []);
-    app.controller("indexController", ['$scope', function($scope) {
-        $scope.appData = ["A", "B", "C"];
-    }]);
-}());
